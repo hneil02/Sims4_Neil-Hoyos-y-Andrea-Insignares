6c70b1aa30adc00ea205a4a881b40a6fd2a5e797
“hneil02” <hneil@uninorte.edu.co>
2020-11-21 19:01:19 -0500

    taller grupal diseño web

---
+++ SRC/JS/acerca.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    M.AutoInit();
+});
+
+const accordion = document.getElementsByClassName('contentBx');
+
+for (i = 0; i<accordion.length; i++ ){
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+}
